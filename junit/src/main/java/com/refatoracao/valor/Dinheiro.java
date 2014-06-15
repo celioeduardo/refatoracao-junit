da67da13816245bdaec3cb44e023c21cd6eaafd0
barnstricolor <barnstricolor@gmail.com>
2014-06-15 18:30:45 -0300

    Primeiros testes Padrao Moeda

--- junit/src/main/java/com/refatoracao/valor/Dinheiro.java
+++ junit/src/main/java/com/refatoracao/valor/Dinheiro.java
@@ -10,26 +10,31 @@ public class Dinheiro {
 	public Dinheiro(double quantia, Currency moeda){
 		this.moeda = moeda;		
 		this.quantia = Math.round(quantia * fatorCentavos());
-	
 	}
 	public Dinheiro(long quantia, Currency moeda){
 		this.moeda = moeda;		
 		this.quantia = quantia * fatorCentavos();	
+	}
+	public Dinheiro(double quantia){
+		this.moeda = Currency.getInstance("BRL");		
+		this.quantia = Math.round(quantia * fatorCentavos());
 	}
 	public Dinheiro() {
 	}
 	public Currency moeda(){
 		return moeda;
 	}
-	
+	public long valor(){
+		return this.quantia;
+	}
 
 	public Dinheiro adicionar(Dinheiro novaQuantia){
 		assercaoMesmaMoedaQue(novaQuantia);
-		return novoDinheiro(quantia + novaQuantia.quantia);
+		return novoDinheiro(this.quantia + novaQuantia.quantia);
 	}
 	public Dinheiro subtrair(Dinheiro novaQuantia){
 		assercaoMesmaMoedaQue(novaQuantia);
-		return novoDinheiro(quantia - novaQuantia.quantia);
+		return novoDinheiro(this.quantia - novaQuantia.quantia);
 	}
 	public int compararCom(Object outro){
 		return compararCom((Dinheiro) outro);
@@ -48,7 +53,8 @@ public class Dinheiro {
 		return dinheiro;
 	}
 	private void assercaoMesmaMoedaQue(Dinheiro outro) {
-		throw new IllegalArgumentException(this.moeda + " diferente " + outro.moeda);		
+		if (this.moeda().equals(outro))
+			throw new IllegalArgumentException(this.moeda + " diferente " + outro.moeda);		
 	}
 	public static Dinheiro emDolares(double quantia){
 		return new Dinheiro(quantia, Currency.getInstance("USD"));
@@ -59,10 +65,12 @@ public class Dinheiro {
 	
 	
 	public boolean equals (Object outro){
-		return (outro instanceof Dinheiro ) && equals((Dinheiro)outro);
+		return (outro instanceof Dinheiro ) && 
+				equals((Dinheiro)outro);
 	}
 	public boolean equals (Dinheiro outro){
-		return moeda.equals(outro.moeda) && (quantia == outro.quantia);
+		return moeda.equals(outro.moeda) && 
+				(quantia == outro.quantia);
 	}
 	public int hashCode(){
 		return (int) (quantia ^ (quantia >>> 32));
@@ -71,5 +79,11 @@ public class Dinheiro {
 	private static final int[] centavos = new int[]{1, 10, 100, 1000};
 	private int fatorCentavos(){
 		return centavos[moeda.getDefaultFractionDigits()];
+	}
+	public long quantia() {
+		return Math.round(quantia / fatorCentavos());
+	}
+	public void vezes(int i) {
+		this.quantia*=i;
 	}
 }    
--- junit/src/test/java/com/refatoracao/valor/DinheiroTest.java
+++ junit/src/test/java/com/refatoracao/valor/DinheiroTest.java
@@ -1,18 +1,34 @@
 package com.refatoracao.valor;
 
 import static org.junit.Assert.*;
+
+import java.util.Currency;
+
 import org.junit.Test;
 
 public class DinheiroTest {
 
 	@Test
-	public void moedaReal(){
-
-		Dinheiro cemReais = Dinheiro.emReais(100);
-		
-		assertEquals(100, cemReais);
-		
+	public void novo(){
+		Dinheiro grana = new Dinheiro(100);		
+		assertEquals(100, grana.quantia());
 	}
-	
-	
+	@Test
+	public void somarDinheiro(){
+		Dinheiro grana = new Dinheiro(100);
+		grana = grana.adicionar(new Dinheiro(350));
+		assertEquals(100+350, grana.quantia());
+	}
+	@Test
+	public void subtrairDinheiro(){
+		Dinheiro grana = new Dinheiro(350);
+		grana = grana.subtrair(new Dinheiro(100));
+		assertEquals(350-100, grana.quantia());
+	}
+	@Test
+	public void novoVezesCinco(){
+		Dinheiro grana = new Dinheiro(100);
+		grana.vezes(5);
+		assertEquals(5*100, grana.quantia());
+	}
 }
