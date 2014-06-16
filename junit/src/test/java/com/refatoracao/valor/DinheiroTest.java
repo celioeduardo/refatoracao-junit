63fa105102bb53ebb5655974933c12511526b265
barnstricolor <barnstricolor@gmail.com>
2014-06-15 22:44:20 -0300

    Correção nos asserts dos testes para boolean com equals

--- junit/src/test/java/com/refatoracao/valor/DinheiroTest.java
+++ junit/src/test/java/com/refatoracao/valor/DinheiroTest.java
@@ -11,24 +11,36 @@ public class DinheiroTest {
 	@Test
 	public void novo(){
 		Dinheiro grana = new Dinheiro(100);		
-		assertEquals(100, grana.quantia());
+		assertTrue(new Dinheiro(100).equals(grana));
 	}
 	@Test
 	public void somarDinheiro(){
 		Dinheiro grana = new Dinheiro(100);
 		grana = grana.adicionar(new Dinheiro(350));
-		assertEquals(100+350, grana.quantia());
+		assertTrue(new Dinheiro(450).equals(grana));
 	}
 	@Test
 	public void subtrairDinheiro(){
 		Dinheiro grana = new Dinheiro(350);
 		grana = grana.subtrair(new Dinheiro(100));
-		assertEquals(350-100, grana.quantia());
+		assertTrue(new Dinheiro(250).equals(grana));
 	}
 	@Test
-	public void multiplicador(){
+	public void multiplicarDinheiro(){
 		Dinheiro grana = new Dinheiro(100);
 		grana = grana.vezes(5);
-		assertEquals(5*100, grana.quantia());
+		assertTrue(new Dinheiro(500).equals(grana));
+	}
+	@Test
+	public void igualdadeMoeda(){
+		Dinheiro dolar = Dinheiro.emDolares(100);
+		dolar = dolar.vezes(5);
+		Dinheiro produto = Dinheiro.emDolares(500);
+		assertTrue(produto.equals(dolar));
+		
+		Dinheiro real = Dinheiro.emReais(100);
+		real = real.vezes(5);
+		produto = Dinheiro.emReais(500);
+		assertTrue(produto.equals(real));	
 	}
 }
